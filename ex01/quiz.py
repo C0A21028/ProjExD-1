57bd89b41f04a651395eb3590c3461e8f2e67cdc
okome <c011929257@edu.teu.ac.jp>
2022-06-14 16:35:10 +0900

    問題

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,33 +1,20 @@
 from random import randint as rad
-def shutudai(n):
-    mondai=["サザエのダンナの名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
-    return mondai[n]
+def shutudai():
+    mondai=[["サザエのダンナの名前は？","マスオ","ますお"],
+    ["カツオの妹の名前は？","ワカメ","わかめ"],
+    ["タラオはカツオから見てどんな関係？","甥","おい","甥っ子","おいっこ"]]
+    return mondai
 
 def kaito():
     n=int(rad(0,2))
-    p=shutudai(n)
-    print(p)
+    p=shutudai()
+    print(p[n][0])
     m=str(input("答えるんだ:"))
-    k1=["マスオ","ますお"]
-    k2=["ワカメ","わかめ"]
-    k3=["甥","おい","甥っ子","おいっこ"]
     c=0
-    kai=""
-    if n==0:
-        for i in range(len(k1)):
-            if m==str(k1[i]):
-                kai="正解!!!"
-                c+=1
-    elif n==1:
-        for i in range(len(k2)):
-            if m==str(k2[i]):
-                kai="正解!!!"
-                c+=1
-    elif n==2:
-        for i in range(len(k3)):
-            if m==str(k3[i]):
-                kai="正解!!!"
-                c+=1
+    for i in range(int(len(p[n])-1)):
+        if m==str(p[n][i+1]):
+            kai="正解!!!"
+            c+=1
     if c==0:
         kai="出直してこい"
         c=0
