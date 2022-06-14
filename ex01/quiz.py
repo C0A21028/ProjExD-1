6f93cc71e2b145e46b0d3eee49951cd1472f9f3c
okome <c011929257@edu.teu.ac.jp>
2022-06-14 16:03:08 +0900

    問題

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -4,30 +4,31 @@ def shutudai(n):
     return mondai[n]
 
 def kaito():
-    n=rad(0,2)
+    n=int(rad(0,2))
     p=shutudai(n)
     print(p)
-    n=str(input("答えるんだ:"))
+    m=str(input("答えるんだ:"))
     k1=["マスオ","ますお"]
     k2=["ワカメ","わかめ"]
     k3=["甥","おい","甥っ子","おいっこ"]
     c=0
+    kai=""
     if n==0:
         for i in range(len(k1)):
-            if p==k1[i]:
+            if m==str(k1[i]):
                 kai="正解!!!"
                 c+=1
     elif n==1:
         for i in range(len(k2)):
-            if p==k2[i]:
+            if m==str(k2[i]):
                 kai="正解!!!"
                 c+=1
     elif n==2:
         for i in range(len(k3)):
-            if p==k3[i]:
+            if m==str(k3[i]):
                 kai="正解!!!"
                 c+=1
-    if c!=0:
+    if c==0:
         kai="出直してこい"
         c=0
     return kai
